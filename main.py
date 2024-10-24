06d2bdb76e0c05e385cda5b0fbee75c6665a0815
msquires22 <msquires@princeton.edu>
2024-10-23 20:40:06 -0400

    tonight

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 #imports
-
-
-
+import pyaudio
+import wave
+import sys
 
 # Input age and genre choice
 age = int(input("Enter user's age: "))
-#print(f"User's age is: {age}")
+print(f"User's age is: {age}")
 
 genres = {1: 'Country', 2: 'Pop', 3: 'Rock', 4: 'Rap', 5: 'Electronic Dance'}
 choice = int(input(f"User's preferred genre: {', '.join(f'{k}. {v}' for k, v in genres.items())}: "))
-#print(f"User's preferred genre is: {genres.get(choice, 'Invalid choice')}")
-
-
-
+print(f"User's preferred genre is: {genres.get(choice, 'Invalid choice')}")
 
 # Calculate Ranges for HR
 maxHR = int(191.5 - .007 * age**2) # max heart rate
-#print(f"Users max heart rate is: {maxHR}")
+print(f"Users max heart rate is: {maxHR}")
 
 maxTHRZ_HI = int(maxHR * .9) # Target Heart Rate Zone Max (High Intensity Interval)
 minTHRZ_HI = int(maxHR * .85) # THRZ Min - HII
@@ -27,19 +24,30 @@ minTHRZ_LI = int(maxHR * .65) # THRZ Min - LII
 maxTHRZ_WC = int(maxHR * .7) # Target Heart Rate Zone Max (Warmup/Cooldown)
 minTHRZ_WC = int(maxHR * .6) # THRZ Min - WC
 
+#################
+
+#Calls to next song, can do this in prototype #2
+
+#################
+
+print("Tell user to begin warming up")
+
+#fetch heart rate data
 
 
-# Calculate Stretch Ranges for Song
+songs = {"music/nomoney.wav", "music/titanium.wav", "music/closer.wav"}
+
+for file in songs:
+    song = wave.open(file, "rb")
+    
+    # Instantiate PyAudio
+    p = pyaudio.PyAudio()
+
+    # Define initial playback speed 
+    speed = 1.0
 
 
 
-
-
-
-
-
-#helper function whcih can detemrine the next song with bpm closest to current HRavg,
-#could call it within the last 5 seconds of a song, or when it gets within 1-2 beats of stretch range max/min
 
 
 
---
+++ nextSong.py
@@ -0,0 +1,5 @@
+# Calculate Stretch Ranges for Song
+#DO THIS LATER
+
+#helper function whcih can detemrine the next song with bpm closest to current HRavg,
+#could call it within the last 5 seconds of a song, or when it gets within 1-2 beats of stretch range max/min
